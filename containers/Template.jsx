cc57c4639458bee3acd609bb4f5d3ddeac6c8ae3
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-11-16 10:54:28 +0900

    template入れ忘れ

---
+++ containers/Template.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
+import MyRawTheme from '../components/materialUiRawThemeFile';
+
+const title = 'Miyahira Livestream';
+
+const Template = props => (
+  <MuiThemeProvider muiTheme={getMuiTheme(MyRawTheme)}>
+    <div>
+      <Helmet
+        defaultTitle={title}
+        titleTemplate={`%s - ${title}`}
+      />
+      {props.children}
+    </div>
+  </MuiThemeProvider>
+);
+
+Template.propTypes = {
+  children: React.PropTypes.shape(),
+};
+
+export default Template;
+
--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
-import Template from '../containers/Template';
 import MyRawTheme from '../components/materialUiRawThemeFile';
+import Template from '../containers/Template';
 import App from '../containers/App';
 
 window.React = React;
