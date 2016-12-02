fa4b7b564fbfe7eb9211071b555119b51739c047
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-02 18:10:28 +0900

    GA導入

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
@@ -8,11 +9,17 @@ import Template from '../containers/Template';
 import App from '../containers/App';
 
 window.React = React;
+ReactGA.initialize('UA-88356342-1');
+
+const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
 
 injectTapEventPlugin();
 ReactDOM.render((
   <MuiThemeProvider muiTheme={getMuiTheme(MyRawTheme)}>
-    <Router history={browserHistory}>
+    <Router history={browserHistory} onUpdate={logPageView}>
       <Route path="/" component={Template}>
         <IndexRoute component={App} />
         <Route path="/:userName" component={App} />
