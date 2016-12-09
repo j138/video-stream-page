d87ce8d217342d2ae3bbaa18464d5fca795f9c83
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-09 10:46:53 +0900

    リファクタ

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   context: __dirname,
   entry: {
-    png: './src/icon.png',
     json: './src/config.json',
     jsx: './src/index.jsx',
     css: ['./src/main.css', './src/videojs-custom.css'],
@@ -21,7 +20,7 @@ module.exports = {
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'eslint-loader' },
     ],
     loaders: [
-      { test: /\.(png|jpg)$/, loader: 'file-loader?name=[name].[ext]' },
+      { test: /\.(png|jpg)$/, loader: 'file?name=[name].[ext]' },
       { test: /\.json$/, loader: 'file?name=[name].[ext]' },
       { test: /\.html$/, loader: 'file?name=[name].[ext]' },
       { test: /\.css$/, loader: 'file?name=[name].[ext]' },
