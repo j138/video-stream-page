551071127ce5d2923807e73552eb5735aa7e96b4
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2016-12-09 18:21:49 +0900

    css-loaderで結合

---
+++ webpack.config.babel.js
@@ -0,0 +1,44 @@
+import 'babel-polyfill';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+const extractCSS = new ExtractTextPlugin('styles.css');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    json: ['./src/users.json'],
+    jsx: './src/index.jsx',
+    css: ['./src/main.css', './src/videojs-custom.css'],
+    html: './src/index.html',
+    // static: ['./src/.htaccess'],
+  },
+  output: {
+    path: `${__dirname}/public`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+  module: {
+    preLoaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'eslint' },
+    ],
+    loaders: [
+      { test: /\.css$/, loader: extractCSS.extract('css') },
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.jpe?g$|\.png$/, loader: 'url' },
+      { test: /\.gif$|\.svg$|\.woff$|\.ttf$|\.html$|\.swf$|\.htaccess$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot-loader/webpack', 'babel'] },
+    ],
+  },
+  plugins: [
+    extractCSS,
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  eslint: {
+    configFile: './.eslintrc',
+  },
+};
