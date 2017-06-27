bbf172bd43458c1d28656d1c8bb3b8d37f712ef0
Jey <j138cm@gmail.com>
2017-06-28 00:01:08 +0900

    fixed techorder

--- components/VideoSection.jsx
+++ components/VideoSection.jsx
@@ -19,7 +19,7 @@ class VideoSection extends React.Component {
     const datasetup = `
         {
           "nativeControlsForTouch": "true",
-          "techOrder": ["flash", "html5"],
+          "techOrder": ["html5"],
           "controlBar": {
             "timeDivider": false,
             "durationDisplay": true,
